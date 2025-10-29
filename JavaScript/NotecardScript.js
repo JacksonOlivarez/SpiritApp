79785251ffbac8738cf58736e698f7c8f32eb320
JacksonOlivarez <142529726+JacksonOlivarez@users.noreply.github.com>
2025-10-29 10:42:37 -0500

    Added Notecards

---
+++ JavaScript/NotecardScript.js
@@ -0,0 +1,24 @@
+
+allNotecardSets = [];
+exampleNotecards = {};
+blankNotecards = {};
+allNotecardSets.push(exampleNotecards);
+allNotecardSets.push(blankNotecards);
+
+
+// Test for now, fill out with more useful notecards    
+exampleNotecards["Card One"] = "Response One";
+exampleNotecards["Card Two"] = "Response Two";
+
+function initialize(){
+    // Create the initial notecards. Only use once per startup
+
+}
+
+// Function to pick a set to focus on, return the dictionary of notecards
+
+// Function to create a new Notecard set
+
+// Function to remove a notecard set
+
+// Function to edit a notecard set (take input from HTML?)
